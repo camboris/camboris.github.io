47d85929ce372ab019828ecd1facb3ecec9b4e39
Mario G. Pozzo <mpozzo@kinexo.com>
2016-03-27 18:42:41 -0300

    Agrego comentarios con Disqus

--- conf.py
+++ conf.py
@@ -831,7 +831,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "camborisblog"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
